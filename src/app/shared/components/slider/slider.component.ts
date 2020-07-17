7b2f9ac5db4ad1a23abdbc494481607206261651
dyhaz <muhammad@visionet.co.id>
2020-07-18 04:03:28 +0700

    fixed disappearing slider

--- src/app/shared/components/slider/slider.component.ts
+++ src/app/shared/components/slider/slider.component.ts
@@ -1,9 +1,23 @@
-import {Component} from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+declare var $: any;
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html'
 })
-export class SliderComponent {
+export class SliderComponent implements OnInit {
   constructor() {}
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      $('.slider-slide').owlCarousel({
+        items: 1,
+        slideSpeed: 1000,
+        dots: true,
+        paginationSpeed: 400,
+        singleItem: true,
+        loop: true,
+        margin: 10
+      });
+    }, 100);
+  }
 }
